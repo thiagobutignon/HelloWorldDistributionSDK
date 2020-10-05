b85e6a2a96eb516ecdb54029011d63e38189163b
Thiago Butignon <thiagobutignon@gmail.com>
2020-10-05 01:01:54 -0300

    add swift package manager

--- Package.swift
+++ Package.swift
@@ -4,19 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "HelloWorldDistributionSDK",
+    name: "HelloWorldSDK",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "HelloWorldSDK",
+            targets: ["HelloWorldSDK"]
+        )
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/thiagobutignon/HelloWorldDistributionSDK.git", from: "0.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "HelloWorldDistributionSDK",
+            name: "HelloWorldSDK",
             dependencies: []),
         .testTarget(
-            name: "HelloWorldDistributionSDKTests",
-            dependencies: ["HelloWorldDistributionSDK"]),
+            name: "HelloWorldSDKTests",
+            dependencies: ["HelloWorldSDK"]),
     ]
 )
