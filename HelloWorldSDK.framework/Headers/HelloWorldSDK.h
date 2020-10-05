806c8ee69de2abd8bc5355d5f37ee8639bd8bed5
Thiago Butignon <thiagobutignon@gmail.com>
2020-10-05 00:25:56 -0300

    Initial Commit

---
+++ HelloWorldSDK.framework/Headers/HelloWorldSDK.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldSDK.h
+//  HelloWorldSDK
+//
+//  Created by Thiago B Claramunt on 04/10/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorldSDK.
+FOUNDATION_EXPORT double HelloWorldSDKVersionNumber;
+
+//! Project version string for HelloWorldSDK.
+FOUNDATION_EXPORT const unsigned char HelloWorldSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldSDK/PublicHeader.h>
+
+
